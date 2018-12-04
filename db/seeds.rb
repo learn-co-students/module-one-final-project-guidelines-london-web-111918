9ba674b9d3effa84efe96ed0d8dc119fb4585af6
IMH51 <ianmartinharrison@outlook.com>
2018-12-04 13:56:01 +0000

    used api data to create unique list of team names in alphabetical order

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,14 +14,23 @@ match_array = response_hash["matches"]
 matches = []
 matches = match_array.map do |match_hash|
               new_match = {}
+              new_match["date"] = match_hash["utcDate"][0..9]
               new_match["team1"] = match_hash["homeTeam"]["name"].gsub("AFC", "").gsub("FC", "").strip
               new_match["team2"] = match_hash["awayTeam"]["name"].gsub("AFC", "").gsub("FC", "").strip
-              new_match["team1_goals"] = match_hash["score"]["fullTime"]["homeTeam"]
-              new_match["team2_goals"] = match_hash["score"]["fullTime"]["awayTeam"]
+              new_match["team1goals"] = match_hash["score"]["fullTime"]["homeTeam"]
+              new_match["team2goals"] = match_hash["score"]["fullTime"]["awayTeam"]
               matches << new_match
             end
 
 match_list = matches[0].uniq
+
+team_names = []
+team_names = match_list.map do |match_hash|
+                  team_names << match_hash["team1"]
+                  team_names << match_hash["team2"]
+                end
+
+team_list = team_names[0].uniq.sort
 
 binding.pry
 0
