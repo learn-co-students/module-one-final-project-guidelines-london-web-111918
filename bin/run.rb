80d52a6f044dc498610ee17ed864528ec8668d74
NathanB09 <nathanbell09@gmail.com>
2018-12-04 10:33:26 +0000

    removed link to api from file

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,5 @@
 require_relative '../config/environment'
 
-all_spells = RestClient.get("https://www.potterapi.com/v1/spells?key=$2a$10$0ewomlY54wR7s9seLoIPseKIfjkMu6m.2EAtv52OpfgEYGAGwWlRu")
-all_spells_json = JSON.parse(all_spells)
+
 
 puts "HELLO WORLD"
