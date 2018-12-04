8604671dd7eefdbf6c0619eeedc252d75d8c4b66
NathanB09 <nathanbell09@gmail.com>
2018-12-04 15:27:07 +0000

    added first list option  and created method for it

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,18 +1,47 @@
+require_relative '../config/environment'
+
 class CommandLineInterface
 
   def run
     welcome
-  end
-
-  def link_api
-    all_spells = RestClient.get("https://www.potterapi.com/v1/spells?key=$2a$10$0ewomlY54wR7s9seLoIPseKIfjkMu6m.2EAtv52OpfgEYGAGwWlRu")
-    JSON.parse(all_spells)
+    menu
   end
 
   def welcome
-    puts "World of Spells"
+    puts <<-TITLE
+    ,--.   ,--.              ,--.   ,--.            ,---.     ,---.                ,--.,--.
+    |  |   |  | ,---. ,--.--.|  | ,-|  |     ,---. /  .-'    '   .-'  ,---.  ,---. |  ||  | ,---.
+    |  |.'.|  || .-. ||  .--'|  |' .-. |    | .-. ||  `-,    `.  `-. | .-. || .-. :|  ||  |(  .-'
+    |   ,'.   |' '-' '|  |   |  || `-' |    ' '-' '|  .-'    .-'    || '-' '|   --.|  ||  |.-'  `)
+    '--'   '--' `---' `--'   `--' `---'      `---' `--'      `-----' |  |-'  `----'`--'`--'`----'
+                                                                     `--'
+    TITLE
     puts "Wizard / Witches name:"
     @users_name = gets.chomp
+    puts "Welcome #{@users_name}!"
+    create_user
+  end
+
+  def create_user
+    User.create(name: @users_name)
+  end
+
+  def menu
+    puts "Please select one of the following options:"
+    puts <<-MENU1
+      1. List all spells
+    MENU1
+    user_input = gets.chomp
+    case user_input
+    when "1"
+      show_all_spells
+    end
+  end
+
+  def show_all_spells
+    Spell.all.each do |spell|
+      puts "#{spell.id}. name: #{spell.name}, type: #{spell.spell_type}, effect: #{spell.effect}"
+    end
   end
 
 end
