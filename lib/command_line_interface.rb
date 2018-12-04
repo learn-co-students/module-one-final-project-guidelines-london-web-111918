3a7f7f97415f42795439f563df113f2adb90574a
NathanB09 <nathanbell09@gmail.com>
2018-12-04 10:51:22 +0000

    added welcome message method

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,18 @@
 class CommandLineInterface
 
+  def run
+    welcome
+  end
+
   def link_api
     all_spells = RestClient.get("https://www.potterapi.com/v1/spells?key=$2a$10$0ewomlY54wR7s9seLoIPseKIfjkMu6m.2EAtv52OpfgEYGAGwWlRu")
     JSON.parse(all_spells)
+  end
+
+  def welcome
+    puts "World of Spells"
+    puts "Wizard / Witches name:"
+    @users_name = gets.chomp
   end
 
 end
