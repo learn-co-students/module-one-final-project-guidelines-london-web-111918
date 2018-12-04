77e88d5e9e9ea3a528f88c2cbfe6734f52cf7d39
NathanB09 <nathanbell09@gmail.com>
2018-12-04 10:26:23 +0000

    added api connection

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,6 @@
 require_relative '../config/environment'
 
-
+all_spells = RestClient.get("https://www.potterapi.com/v1/spells?key=$2a$10$0ewomlY54wR7s9seLoIPseKIfjkMu6m.2EAtv52OpfgEYGAGwWlRu")
+all_spells_json = JSON.parse(all_spells)
 
 puts "HELLO WORLD"
