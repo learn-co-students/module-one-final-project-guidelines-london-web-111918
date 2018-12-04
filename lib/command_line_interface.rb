142d59b47f8a7159f223591091372df60872d5a4
NathanB09 <nathanbell09@gmail.com>
2018-12-04 15:38:01 +0000

    added second menu option and created method for finding a spell

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -19,7 +19,7 @@ class CommandLineInterface
     puts "Wizard / Witches name:"
     @users_name = gets.chomp
     puts "Welcome #{@users_name}!"
-    create_user
+    create_user ; nil
   end
 
   def create_user
@@ -30,11 +30,14 @@ class CommandLineInterface
     puts "Please select one of the following options:"
     puts <<-MENU1
       1. List all spells
+      2. Find spell by name
     MENU1
     user_input = gets.chomp
     case user_input
     when "1"
       show_all_spells
+    when "2"
+      find_spell
     end
   end
 
@@ -42,6 +45,13 @@ class CommandLineInterface
     Spell.all.each do |spell|
       puts "#{spell.id}. name: #{spell.name}, type: #{spell.spell_type}, effect: #{spell.effect}"
     end
+  end
+
+  def find_spell
+    puts "Please enter a spell name:"
+    user_input = gets.chomp
+    spell = Spell.find_by(name: user_input)
+    puts "name: #{spell.name}, type: #{spell.spell_type}, effect: #{spell.effect}"
   end
 
 end
