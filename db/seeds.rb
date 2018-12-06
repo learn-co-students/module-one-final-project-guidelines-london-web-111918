a6141ac85124fcef12f57617c41d6336c6cc713d
NathanB09 <nathanbell09@gmail.com>
2018-12-06 12:52:30 +0000

    changed the way the database is seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@ require_relative '../config/environment'
 all_spells = RestClient.get("https://www.potterapi.com/v1/spells?key=$2a$10$0ewomlY54wR7s9seLoIPseKIfjkMu6m.2EAtv52OpfgEYGAGwWlRu")
 spells_json = JSON.parse(all_spells)
 
-spells_json.map do |spell|
-  if !Spell.find_by(name: spell["name"])
+if Spell.all.count == 0
+  spells_json.map do |spell|
     new_spell = Spell.new
     new_spell.name = spell["spell"]
     new_spell.spell_type = spell["type"]
