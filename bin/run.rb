6be6a015b67cbce62022f1df61dc68c8da612a25
NathanB09 <nathanbell09@gmail.com>
2018-12-06 23:28:37 +0000

    changed comment info

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,7 @@
 require_relative '../config/environment'
 
-# uncomment the below lines before you run the program for the first time
+# comment out system after running the program for the first time
+
 # system("bundle &>/dev/null")
 # system("rake db:migrate &>/dev/null")
 # system("rake db:seed")
