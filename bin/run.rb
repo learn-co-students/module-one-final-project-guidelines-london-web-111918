9b73f10f5254bfe6845fd026a2dfd9bd4f3f1949
NathanB09 <nathanbell09@gmail.com>
2018-12-06 17:37:17 +0000

    added comment to file

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,9 @@
 require_relative '../config/environment'
 
-system("bundle &>/dev/null")
-system("rake db:migrate &>/dev/null")
-system("rake db:seed")
+# uncomment the below lines before you run the program for the first time
+# system("bundle &>/dev/null")
+# system("rake db:migrate &>/dev/null")
+# system("rake db:seed")
 
 cli = CommandLineInterface.new
 cli.run
